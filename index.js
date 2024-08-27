3c19763e2dddf10711aac8bd0d34eb41a8496de5
Faisal Memon <faisal.memon1991@gmail.com>
2024-08-27 19:51:44 +0530

    var vs let vs const

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-// RULES FOR NAMING VARIABLES
-// * Must begin with a letter, $, or _.
-// * Can contain letters, digits, $, and _ after the 
-//          first character.
-// * Case-sensitive.
-// * Cannot use reserved keywords.
-// * No spaces allowed.
-// * Use descriptive names.
+// In JavaScript, you create a variable using the let, const, or var 
+//    keyword.
+// let: Use this when you might want to change the value
+// const: Use this when the value should stay the same.
+// var: An older way of creating variables, 
+//       but now we usually use let or const.
 
-let count = 10;
-let $price = 5;
-let _name = "Hi";
-// let 1name = "name";
+var counter = 100;
+console.log(counter);
 
-let u1$ser1;
-let first_name;
+let newCounter = 100;
+console.log(newCounter);
+newCounter = 200;
+console.log(newCounter);
 
-let name = "Alice";
-let Name = "Tom";
-console.log(name);
-console.log(Name);
-
-// let let = 10;
-
-let firstName = 10;
-
-let age = 22;
-
-
-// let a1 = 5, a2 = 10, a3 = 15;
-// console.log(a1);
-// console.log(a2);
-// console.log(a3);
-
-let a1 = 5;
-let a2 = 10;
-let a3 = 15;
-
-let x, y, z;
-x = y = z = 30;
-console.log(x);
-console.log(y);
-console.log(z);
+const PI = 3.14;
+// PI = 400;
+console.log(PI);
