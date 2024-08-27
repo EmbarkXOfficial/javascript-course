c81b22e26acedafc29f5574e5197490ef4406931
Faisal Memon <faisal.memon1991@gmail.com>
2024-08-27 14:52:40 +0530

    Seperate js file with comments

---
+++ index.js
@@ -0,0 +1,21 @@
+// This is a single line comment.
+console.log("Hello world!!!...");
+console.log("Hello world!!!...");
+
+
+/* 
+This is multi line comment
+console.log("Hello world!!!...");
+console.log("Hello world!!!..."); */
+
+
+/**
+ * Adds two numbers together.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function add(a, b) {
+    return a + b;
+}
