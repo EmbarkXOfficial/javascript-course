1a4710af3ac7de4094cd844cb9ef6955e1cb5354
Faisal Memon <faisal.memon1991@gmail.com>
2024-08-27 19:34:53 +0530

    Variable Names

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-// WHAT IS A VARIABLE?
-//  1. A variable is like a storage box where you can keep a piece 
-// of information to use later.
-//  2. You can think of it as a label that you stick on something you 
-// want to remember.
-
+// RULES FOR NAMING VARIABLES
+// * Must begin with a letter, $, or _.
+// * Can contain letters, digits, $, and _ after the 
+//          first character.
+// * Case-sensitive.
+// * Cannot use reserved keywords.
+// * No spaces allowed.
+// * Use descriptive names.
 
 let count = 10;
-console.log(count);
-console.log(count);
-console.log(count);
-count = 40;
-console.log(count);
-count = 80;
-console.log(count);
-console.log(count + 10);
+let $price = 5;
+let _name = "Hi";
+// let 1name = "name";
+
+let u1$ser1;
+let first_name;
+
+let name = "Alice";
+let Name = "Tom";
+console.log(name);
+console.log(Name);
+
+// let let = 10;
+
+let firstName = 10;
+
+let age = 22;
+
+
+// let a1 = 5, a2 = 10, a3 = 15;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+
+let a1 = 5;
+let a2 = 10;
+let a3 = 15;
+
+let x, y, z;
+x = y = z = 30;
+console.log(x);
+console.log(y);
+console.log(z);
