673fc04704f7d976a3854f5b00a87ef27a1f9318
Faisal Memon <faisal.memon1991@gmail.com>
2024-08-29 11:39:55 +0530

    Statically or Dynamically Typed

--- index.js
+++ index.js
@@ -1,54 +1,26 @@
-// DATATYPES
-// Define the kind of value variable is holding
+// STATICALLY TYPED LANGUAGE Example: Java
+// Variables cannot change their type during 
+// the program's execution
 
-// PRIMITIVE TYPES
-// Number: Represents both integers and floating 
-//          point numbers
+// int age = 30;
+// age = "Java";
 
-let age = 70;
-console.log(typeof age);
-console.log(age);
 
-// String: Represents sequence of characters
-let name = "John";
-console.log(typeof name);
+// JAVASCRIPT IS DYNAMICALLY TYPED LANGUAGE
+// Variables can change their type during 
+// the program's execution
 
-let city = 'San Jose';
-console.log(typeof city);
+let myVariable;
+console.log(typeof myVariable);
 
-console.log(name + " " + city);
+myVariable = 1;
+console.log(typeof myVariable);
+console.log(myVariable);
 
-// Boolean: Represents true or false
-let hasGraduated = true;
-console.log(typeof hasGraduated);
+myVariable = "Javascript";
+console.log(typeof myVariable);
+console.log(myVariable);
 
-// Undefined: No value assigned
-let area;
-console.log(typeof area);
-
-// Null: Represents intentional absence of value
-let emptyValue = null;
-console.log(emptyValue);
-
-// REFERENCE TYPES
-// Object: Used to store complex data in form 
-//          of key value pairs
-let userAge = 22;
-let userName = "John";
-
-let user = {
-    name: "John",
-    age: 22,
-    city: "San Francisco"
-}
-console.log(user.age);
-console.log(user.name);
-user.name = "Alice";
-console.log(user.name);
-console.log(user);
-
-// Array: Used to hold multiple values
-let fruit = ["Apple", "Banana", "Strawberry"]
-console.log(fruit);
-console.log(typeof fruit);
-
+myVariable = true;
+console.log(typeof myVariable);
+console.log(myVariable);
