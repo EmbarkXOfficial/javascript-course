d8e2e6259749900cfb05796b6d31004b1ab6e836
Faisal Memon <faisal.memon1991@gmail.com>
2024-08-30 17:45:53 +0530

    Ternary Operator

--- index.js
+++ index.js
@@ -1,114 +1,24 @@
 /*
-Logical Operators:
-   - Logical AND (&&)
-   - Logical OR (||)
-   - Logical NOT (!)
+Ternary Operator:
+   - Conditional operator 
+   (condition ? value1 : value2)
 */
 
-
-/* 
-1. Logical AND (&&)
-   - The AND operator (&&) checks if BOTH 
-   conditions are true.
-   - It returns true only if both are true, 
-   otherwise, it returns false.
-*/
-
-console.log(true && true);
-console.log(true && false);
-console.log(false && true);
-console.log(false && false);
-
-// Example
-let hasTickets = true;
-let hasId = true;
-console.log(hasTickets && hasId)
-
-hasId = false;
-console.log(hasTickets && hasId)
-
-console.log((5 > 3) && (5 > 6));
+let x = 4;
+let message = (x > 5) ? "x is > 5" : "x is < 5";
+console.log(message);
 
 
+let number = 5;
+let result = (number % 2 === 0) ? "even" : "odd";
+console.log(result);
 
-/*
-2. Logical OR (||)
-   - The OR operator (||) checks if AT LEAST 
-   ONE condition is true.
-   - It returns true if at least one value is 
-   true; if both are false, it returns false.
-*/
+// Nested Ternary Operator
+// age < 13 -> Child
+// age 13-20 -> Teenager
+// age > 20 -> Adult
 
-
-console.log(true || true);
-console.log(true || false);
-console.log(false || true);
-console.log(false || false);
-
-
-let isSunny = false;
-let hasRaincoat = true;
-console.log(isSunny || hasRaincoat);
-hasRaincoat = false;
-console.log(isSunny || hasRaincoat);
-
-
-
-
-/*
-3. Logical NOT (!)
-   - The NOT operator (!) inverts the value 
-   of a Boolean.
-   - It turns true into false and false into 
-   true.
-*/
-
-console.log(!true);
-
-let isRaining = true;
-let shouldStayInside = !isRaining;
-console.log(shouldStayInside);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-5. Unary Operators:
-   - Increment (++)
-   - Decrement (--)
-   - typeof operator
-   - Logical NOT (!)
-
-6. Ternary Operator:
-   - Conditional operator (condition ? value1 : value2)
-*/
+let age = 22;
+let category = (age < 13) ? "Child" : 
+        (age < 20 ) ? "Teenager" : "Adult";
+console.log(category);
