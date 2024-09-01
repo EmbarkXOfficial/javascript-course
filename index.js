2bb331ba20906006bd9013dc4ce5725da22b5a5c
Faisal Memon <faisal.memon1991@gmail.com>
2024-09-01 12:34:43 +0530

    Objects in JavaScript

--- index.js
+++ index.js
@@ -1,75 +1,57 @@
 /*
-1. Arrays
-   Arrays are used to store multiple values in 
-   a single variable.
+JavaScript Objects
 
-   Creating Arrays:
-   You can create an array by enclosing its 
-   elements within square brackets ([]).
+Objects allow you to group related data and 
+functions together. 
+
+An object is created using curly braces `{}` 
+and contains key-value pairs, where each key 
+(also called a property) is associated with 
+a value.
+
 */
 
-let numbers = [1, 2, 3, 4]
-console.log(numbers);
+let person = {
+        name: "Alice",
+        age: 25,
+        isStudent: true
+}
+console.log(person);
 
-let fruits = ["apple", "banana", "orange"]
-console.log(fruits);
+// Accessing Properties
+console.log(person.age);
+console.log(person["age"]);
 
-// [1, 2, 3, 4]
-//  0  1  2  3
-console.log(numbers[3]);
-console.log(fruits[1]);
+// Adding or Updating Properties
+person.age = 66;
+console.log(person.age);
+console.log(person);
 
-fruits[1] = "cherry";
-console.log(fruits[1]);
+person.job = "Engineer";
+console.log(person);
+console.log(person.job);
 
-let matrix = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
-]
-console.log(matrix[0]);
-console.log(matrix[0][1]);
+// Deleting Properties
+delete person.isStudent;
+console.log(person);
+
+// Check if property exist
+console.log('name' in person);
+console.log(person.hasOwnProperty("age"));
 
 
-/*
-push(): Adds an element to the end.
-pop(): Removes the last element.
-shift(): Removes the first element.
-unshift(): Adds an element to the beginning.
-slice(): Copies a portion of an array.
-splice(): Adds or removes elements at a 
-        specific index.
-*/
+// Nested Objects
+let student = {
+        name: "Bob",
+        courses : {
+                math: true,
+                science: false
+        }
+}
+console.log(student);
+console.log(student.courses.math);
 
-console.log(fruits.length);
-
-// push()
-fruits.push("banana");
-console.log(fruits);
-console.log(fruits.length);
-
-// pop()
-let lastFruit = fruits.pop();
-console.log(fruits);
-console.log(lastFruit);
-
-// shift()
-let firstFruit = fruits.shift();
-console.log(fruits);
-console.log(firstFruit);
-
-// unshift()
-let newLength = fruits.unshift("apple again");
-console.log(fruits);
-console.log(newLength);
-
-// slice()
-let newFruits = ["apple", "banana", "orange", 
-        "strawberry"]
-let slicedArray = newFruits.slice(1,3);
-console.log(slicedArray);
-
-// splice()
-let deletedItems = fruits.splice(1,2, "New Item", "New Item Again");
-console.log(fruits);
-console.log(deletedItems);
+// Object Destructuring
+let {name, courses} = student;
+console.log(name);
+console.log(courses);
