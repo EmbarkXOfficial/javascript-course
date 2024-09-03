9744d4654885f51545c293ceb07ee524d0cf5c16
Faisal Memon <faisal.memon1991@gmail.com>
2024-09-03 15:17:20 +0530

    Template literals

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-document.getElementById('convert').onclick = function() {
-  let temp = parseFloat(document.getElementById("temp").value);
-  let unit = document.getElementById("unit").value;
-  
-  let convertedTemp = (unit === 'C')
-  ? (temp * 9/5) + 32 // Convert C to F
-  : (temp - 32) * 5/9 // Convert F to C
+/*
+TEMPLATE LITERALS
+Allows you to work with strings in a more 
+flexible and readable way
 
-  document.getElementById("converted").textContent = convertedTemp.toFixed(2);
+SYNTAX: `${expression}`
+*/
+
+let studentName = 'Alice';
+let age = 17;
+let greeting = `Hello ${studentName}, you are ${age} year old`;
+console.log(greeting);
+
+// Multiline String
+let multilineString =  `This 
+is
+a 
+multiline 
+string`;
+console.log(multilineString)
+
+// Expressions
+let a = 5;
+let b = 10;
+let result = `The sum of ${a} and ${b} is ${a+b}`;
+console.log(result);
+
+// Objects
+let user = {
+  firstName: "John",
+  lastName: "Doe",
+  age:33
 };
+
+let userInfo = `User Info:
+Name: ${user.firstName} ${user.lastName}
+Age: ${user.age}`;
+console.log(userInfo);
+
+
+let e = `This is a backtick \``
+console.log(e);
